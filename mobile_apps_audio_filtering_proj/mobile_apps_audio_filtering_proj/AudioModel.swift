0da45db864fd1bf8d4837471e964baa16596ec85
lattiric <78038846+lattiric@users.noreply.github.com>
2024-10-03 14:29:37 -0500

    Created Projec and novocaine is still not functional

---
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/AudioModel.swift
@@ -0,0 +1,152 @@
+//
+//  AudioModel.swift
+//  AudioLabSwift
+//
+//  Created by Eric Larson 
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+import Foundation
+import Accelerate
+
+class AudioModel {
+    
+    // MARK: Properties
+    private var BUFFER_SIZE:Int
+    // thse properties are for interfaceing with the API
+    // the user can access these arrays at any time and plot them if they like
+    var timeData:[Float]
+    var fftData:[Float]
+    lazy var samplingRate:Int = {
+        return Int(self.audioManager!.samplingRate)
+    }()
+    
+    // MARK: Public Methods
+    init(buffer_size:Int) {
+        BUFFER_SIZE = buffer_size
+        // anything not lazily instatntiated should be allocated here
+        timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
+        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
+    }
+    
+    // public function for starting processing of microphone data
+    func startMicrophoneProcessing(withFps:Double){
+        // setup the microphone to copy to circualr buffer
+        if let manager = self.audioManager{
+//            manager.inputBlock = self.handleMicrophone
+//            manager.inputBlock = self.handleSpeakerQueryWithAudioFile
+            manager.outputBlock = self.handleSpeakerQueryWithAudioFile
+            
+            // repeat this fps times per second using the timer class
+            //   every time this is called, we update the arrays "timeData" and "fftData"
+            Timer.scheduledTimer(withTimeInterval: 1.0/withFps, repeats: true) { _ in
+                self.runEveryInterval()
+            }
+            
+        }
+    }
+    
+    
+    // You must call this when you want the audio to start being handled by our model
+    func play(){
+        if let manager = self.audioManager, let reader=self.fileReader{
+            manager.play()
+            reader.play()
+        }
+    }
+    
+    func pause(){
+        if let manager = self.audioManager, let reader=self.fileReader{
+            manager.pause()
+            reader.pause()
+        }
+    }
+    
+    
+    //==========================================
+    // MARK: Private Properties
+    private lazy var audioManager:Novocaine? = {
+        return Novocaine.audioManager()
+    }()
+    
+    private lazy var fftHelper:FFTHelper? = {
+        return FFTHelper.init(fftSize: Int32(BUFFER_SIZE))
+    }()
+    
+    
+    private lazy var inputBuffer:CircularBuffer? = {
+        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
+                                   andBufferSize: Int64(BUFFER_SIZE))
+    }()
+    
+    private lazy var outputBuffer:CircularBuffer? = {
+        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
+                                   andBufferSize: Int64(BUFFER_SIZE))
+    }()
+    
+    
+    //==========================================
+    // MARK: Private Methods
+    private lazy var fileReader: AudioFileReader? = {
+        print("beats almost dropped")
+        if let url = Bundle.main.url(forResource: "satisfaction", withExtension:"mp3"){
+            var tmpFileReader:AudioFileReader? = AudioFileReader.init(audioFileURL: url, samplingRate: Float(audioManager!.samplingRate), numChannels: audioManager!.numOutputChannels)
+            print("beats almost dropped")
+            tmpFileReader!.currentTime = 0.0
+            print("Audio file succesfully loaded for \(url)")
+            return tmpFileReader
+        }else{
+            print("Could not initialize audio input file")
+            return nil
+        }
+    }()
+    
+    
+    
+    //==========================================
+    // MARK: Model Callback Methods
+    private func runEveryInterval(){
+        if inputBuffer != nil {
+            // copy time data to swift array
+            self.inputBuffer!.fetchFreshData(&timeData, // copied into this array
+                                             withNumSamples: Int64(BUFFER_SIZE))
+            
+            // now take FFT
+            fftHelper!.performForwardFFT(withData: &timeData,
+                                         andCopydBMagnitudeToBuffer: &fftData) // fft result is copied into fftData array
+            
+            // at this point, we have saved the data to the arrays:
+            //   timeData: the raw audio samples
+            //   fftData:  the FFT of those same samples
+            // the user can now use these variables however they like
+            
+        }
+    }
+    
+    //==========================================
+    // MARK: Audiocard Callbacks
+    // in obj-C it was (^InputBlock)(float *data, UInt32 numFrames, UInt32 numChannels)
+    // and in swift this translates to:
+    private func handleMicrophone (data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
+        // copy samples from the microphone into circular buffer
+        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
+    }
+    
+    
+    private func handleSpeakerQueryWithAudioFile(data: Optional<UnsafeMutablePointer<Float>>, numFrames: UInt32, numChannels: UInt32) {
+        if let file = self.fileReader{
+            //read from file, loading into data (a float pointer)
+            
+            if let arrayData = data{
+                file.retrieveFreshAudio(arrayData, numFrames: numFrames, numChannels: numChannels)
+                
+                if inputBuffer != nil {
+                    self.inputBuffer?.addNewFloatData(arrayData, withNumSamples: Int64(numFrames))
+                }
+            }
+            
+        }
+    }
+
+    
+}
---
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  mobile_apps_audio_filtering_proj
+//
+//  Created by Rick Lattin on 10/2/24.
+//
+
+import UIKit
+import Metal
+
+
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userView: UIView!
+    struct AudioConstants{
+        static let AUDIO_BUFFER_SIZE = 1024*4
+    }
+    
+    // setup audio model
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+    lazy var graph:MetalGraph? = {
+        return MetalGraph(userView: self.userView)
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let graph = self.graph{
+            graph.setBackgroundColor(r: 0, g: 0, b: 0, a: 1)
+            
+            // add in graphs for display
+            // note that we need to normalize the scale of this graph
+            // because the fft is returned in dB which has very large negative values and some large positive values
+            
+            
+            graph.addGraph(withName: "fft",
+                            shouldNormalizeForFFT: true,
+                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/2)
+            
+            graph.addGraph(withName: "fft_zoomed",
+                            shouldNormalizeForFFT: true,
+                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE/20)
+            
+            graph.addGraph(withName: "time",
+                numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
+            
+            graph.addGraph(withName: "20 Pts Graph",
+                           shouldNormalizeForFFT: true,
+                           numPointsInGraph: 20)
+            
+            
+            
+            graph.makeGrids() // add grids to graph
+        }
+        
+        // start up the audio model here, querying microphone
+        audio.startMicrophoneProcessing(withFps: 20) // preferred number of FFT calculations per second
+
+        audio.play()
+        
+        // run the loop for updating the graph peridocially
+        Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
+            self.updateGraph()
+        }
+       
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("Paused lol")
+        super.viewDidDisappear(animated)
+        self.audio.pause()
+    }
+    
+    // periodically, update the graph with refreshed FFT Data
+    func updateGraph(){
+        
+        if let graph = self.graph{
+            graph.updateGraph(
+                data: self.audio.fftData,
+                forKey: "fft"
+            )
+            
+            let zoomedArray:[Float] = Array.init(self.audio.fftData[20...])
+            graph.updateGraph(
+                data: zoomedArray,
+                forKey: "fft_zoomed"
+            )
+            
+            graph.updateGraph(
+                data: self.audio.timeData,
+                forKey: "time"
+            )
+
+            var avgdArray: [Float] = Array.init()
+            var chunkSize = self.audio.fftData.count/20
+            for num in 0...(19){
+                avgdArray.append(vDSP.maximum(self.audio.fftData[num*chunkSize...(num*chunkSize)+(chunkSize-1)]))
+            }
+            graph.updateGraph(
+                data: avgdArray,
+                forKey: "20 Pts Graph"
+            )
+            
+        }
+        
+    }
+    
+    
+
+}
+
