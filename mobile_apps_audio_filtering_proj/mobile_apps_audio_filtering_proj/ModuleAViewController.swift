a8384a829f8f8b9051b976967b2fdf1b0b7ea108
lattiric <78038846+lattiric@users.noreply.github.com>
2024-10-06 19:57:00 -0500

    Began working on hz identifiying algorithm. Can read in data and process but cannot find hz yet

--- mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
@@ -16,10 +16,16 @@ class ModuleAViewController: UIViewController {
         static let AUDIO_BUFFER_SIZE = 1024 * 4
     }
     @IBOutlet weak var freq1Label: UILabel!
+    
     @IBOutlet weak var freq2Label: UILabel!
     
     @IBOutlet weak var freq1: UILabel!
+    
     @IBOutlet weak var freq2: UILabel!
+    
+    var cur_hz_1: Double = 0.0
+    var cur_hz_2: Double = 0.0
+    
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
     lazy var graph: MetalGraph? = {
         return MetalGraph(userView: self.userView)
@@ -37,6 +43,8 @@ class ModuleAViewController: UIViewController {
    
         Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
             self.updateGraph()
+        }
+        Timer.scheduledTimer(withTimeInterval: 1.00, repeats: true) { _ in
             self.updateLabels()
         }
     }
@@ -80,17 +88,14 @@ class ModuleAViewController: UIViewController {
     }
     
     func updateLabels() {
-        if let label1 = self.freq1 {
-//            var output_label = calcTone(audio.fftData)
-            var output_label = String(audio.fftData[0])
-            label1.text = output_label + " Hz"
+        if let label1 = self.freq1, let label2 = self.freq2 {
+            self.calcTone(audio_data: audio.fftData)
+            var output_label_1 = String(cur_hz_1)
+            label1.text = output_label_1 + " Hz"
+            var output_label_2 = String(cur_hz_2)
+            label2.text = output_label_2 + " Hz"
         }
-        
-        if let label2 = self.freq2 {
-//            var output_label = calcTone(audio.fftData)
-            var output_label = String(audio.fftData[0])
-            label2.text = output_label + " Hz"
-        }
+    
     }
     
 
@@ -99,8 +104,60 @@ class ModuleAViewController: UIViewController {
         audio.pause()
     }
     
-    func calcTone(audio_data: [Int]) {
-        var data: [Int] = audio_data
+    func calcTone(audio_data: [Float]) {
+        var data: [Float] = audio_data
+        var window: [Float] = []
+        
+        var max_list: [Int: Int] = [:]
+//        var one_max: Int = 0
+        var num_runs: Int = data.count/50
+        
+        for i in 0...(data.count-50) {
+            window = Array(data[(i)...(i)+49])
+
+//            if let win_max = window.max(){
+//                if !win_max.isNaN{
+//                    print(win_max)
+////                    one_max = Int(round(win_max))
+//                } else {
+//                    print("Max value is NaN for some reason????")
+//                }
+//            }
+            //TODO: add these to dict to try and calc frequency
+            var test_max = window.max()
+            var test_index = data.firstIndex(of: test_max!)
+            print("We find " + String(test_max!) + " at " + String(test_index!))
+            
+            
+//            if let (index, winMax) = window.enumerated().max(by: { $0.element < $1.element}){
+////                print(index)
+//                if let max = max_list[index]{
+//                               max_list[index] = max_list[index]!+1
+//                           }else {
+//                               max_list[index] = 1
+//                           }
+//                print("Max Ind count: " + String(max_list[index]!))
+//            }
+        }
+        
+        // Get first highest frequency from dict
+        if let (key, value) = max_list.max(by: { $0.value < $1.value }){
+            cur_hz_1 = Double(key)
+            max_list[key] = 0
+        } else {
+            cur_hz_1 = Double(0.0)
+        }
+        
+        // Get second highest frequency from dict
+        if let (key, value) = max_list.max(by: { $0.value < $1.value }){
+            cur_hz_2 = Double(key)
+            max_list[key] = 0
+        } else {
+            cur_hz_2 = Double(0.0)
+        }
+
+      
+        
     }
     
 }
