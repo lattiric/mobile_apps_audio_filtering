5dd0ea64d2e9f12574462ba144c00e0621054436
lattiric <78038846+lattiric@users.noreply.github.com>
2024-10-07 16:17:03 -0500

    Got the audio to be read within 50hz

--- mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
@@ -45,7 +45,9 @@ class ModuleAViewController: UIViewController {
             self.updateGraph()
         }
         Timer.scheduledTimer(withTimeInterval: 1.00, repeats: true) { _ in
-            self.updateLabels()
+            if(self.audio.fftData[0] > -50.0){
+                self.updateLabels()
+            }
         }
     }
 
@@ -90,9 +92,9 @@ class ModuleAViewController: UIViewController {
     func updateLabels() {
         if let label1 = self.freq1, let label2 = self.freq2 {
             self.calcTone(audio_data: audio.fftData)
-            var output_label_1 = String(cur_hz_1)
+            var output_label_1 = String(Int(cur_hz_1))
             label1.text = output_label_1 + " Hz"
-            var output_label_2 = String(cur_hz_2)
+            var output_label_2 = String(Int(cur_hz_2))
             label2.text = output_label_2 + " Hz"
         }
     
@@ -107,51 +109,54 @@ class ModuleAViewController: UIViewController {
     func calcTone(audio_data: [Float]) {
         var data: [Float] = audio_data
         var window: [Float] = []
-        
         var max_list: [Int: Int] = [:]
-//        var one_max: Int = 0
-        var num_runs: Int = data.count/50
+        var max_list_val: [Int: Float] = [:]
+        var hz_per_index = 44100.0/Double(AudioConstants.AUDIO_BUFFER_SIZE)
+        print(hz_per_index)
         
-        for i in 0...(data.count-50) {
-            window = Array(data[(i)...(i)+49])
+        for i in 1...(data.count-5) {
+            window = Array(data[(i)...(i)+4])
 
-//            if let win_max = window.max(){
-//                if !win_max.isNaN{
-//                    print(win_max)
-////                    one_max = Int(round(win_max))
-//                } else {
-//                    print("Max value is NaN for some reason????")
-//                }
-//            }
-            //TODO: add these to dict to try and calc frequency
-            var test_max = window.max()
-            var test_index = data.firstIndex(of: test_max!)
-            print("We find " + String(test_max!) + " at " + String(test_index!))
-            
-            
-//            if let (index, winMax) = window.enumerated().max(by: { $0.element < $1.element}){
-////                print(index)
-//                if let max = max_list[index]{
-//                               max_list[index] = max_list[index]!+1
-//                           }else {
-//                               max_list[index] = 1
-//                           }
-//                print("Max Ind count: " + String(max_list[index]!))
-//            }
+            if let win_max = window.max(){
+                if let win_index = data.firstIndex(of: win_max){
+                    
+                    if let _ = max_list[win_index]{
+                        max_list[win_index] = max_list[win_index]!+1
+                        max_list_val[win_index] = win_max
+                    }else {
+                        max_list[win_index] = 1
+                        max_list_val[win_index] = win_max
+                    }
+                }
+            }
         }
         
+        //populates all possible peaks
+        var possible_peaks: [Int: Float] = [:]
+        for item in max_list{
+            if item.value >= 5{
+                possible_peaks[item.key] = max_list_val[item.key]
+            }
+        }
+        
+//        print(max_list)
+//        print("break")
+//        print(possible_peaks)
+        
         // Get first highest frequency from dict
-        if let (key, value) = max_list.max(by: { $0.value < $1.value }){
-            cur_hz_1 = Double(key)
-            max_list[key] = 0
+        if let (key, _) = possible_peaks.max(by: { $0.value < $1.value }){
+            cur_hz_1 = Double(key)*hz_per_index
+            print(String(key) + " " + String(cur_hz_1))
+            possible_peaks.removeValue(forKey: key)
         } else {
             cur_hz_1 = Double(0.0)
         }
-        
+                
         // Get second highest frequency from dict
-        if let (key, value) = max_list.max(by: { $0.value < $1.value }){
-            cur_hz_2 = Double(key)
-            max_list[key] = 0
+        if let (key, _) = possible_peaks.max(by: {  $0.value < $1.value }){
+            cur_hz_2 = Double(key)*hz_per_index
+            print(key)
+            
         } else {
             cur_hz_2 = Double(0.0)
         }
