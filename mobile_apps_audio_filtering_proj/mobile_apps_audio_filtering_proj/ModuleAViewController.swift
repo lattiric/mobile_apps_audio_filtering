79098e7ef98c6f2ab5adabba158b8c23c5113eb1
lattiric <78038846+lattiric@users.noreply.github.com>
2024-10-09 01:30:09 -0500

    Attempted grad section fine tuning

--- mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
@@ -29,11 +29,29 @@ class ModuleAViewController: UIViewController {
     var cur_hz_1: Double = 0.0
     var cur_hz_2: Double = 0.0
     var peak_1_index: Int = 0
+    var vowelToggle: Int = 0
+    
+    var peak_sum: Float = 0.0
+    var peak_count: Float = 0.0
+    
+    var harm_sum: Float = 0.0
+    var harm_count: Float = 0.0
     
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
     lazy var graph: MetalGraph? = {
         return MetalGraph(userView: self.userView)
     }()
+    
+    //toggles vowel recognition
+    @IBAction func vowelToggle(_ sender: Any) {
+        if(vowelToggle == 0){
+            vowelToggle = 1
+            self.vowelLabel.text = "Active"
+        } else {
+            vowelToggle = 0
+            self.vowelLabel.text = "Inactive"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,9 +74,12 @@ class ModuleAViewController: UIViewController {
                 self.updateLabels()
                 run_counter = 0
             } else if run_counter > 40{
-                if let label1 = self.freq1, let label2 = self.freq2 {
+                if let label1 = self.freq1, let label2 = self.freq2, let label3 = self.vowelLabel {
                     label1.text = "Noise"
                     label2.text = "Noise"
+                    if(self.vowelToggle == 1){
+                        label3.text = "None Detected Yet (Noise)"
+                    }
                 }
             } else {
                 run_counter = run_counter+1
@@ -92,7 +113,9 @@ class ModuleAViewController: UIViewController {
     func updateLabels() {
         if let label1 = self.freq1, let label2 = self.freq2 {
             self.calcTone(audio_data: audio.fftData)
-            self.calcVowel(audio_data: audio.fftData, peak_index: peak_1_index)
+            if(self.vowelToggle == 1){
+                self.calcVowel(audio_data: audio.fftData, peak_index: peak_1_index)
+            }
             let output_label_1 = String(Int(cur_hz_1))
             label1.text = output_label_1 + " Hz"
             let output_label_2 = String(Int(cur_hz_2))
@@ -114,8 +137,8 @@ class ModuleAViewController: UIViewController {
         var max_list_val: [Int: Float] = [:]
         let hz_per_index = 44100.0/Double(AudioConstants.AUDIO_BUFFER_SIZE)
         
-        for i in 1...(data.count-5) {
-            window = Array(data[(i)...(i)+4])
+        for i in 1...(data.count-6) {
+            window = Array(data[(i)...(i)+5])
 
             if let win_max = window.max(){
                 if let win_index = data.firstIndex(of: win_max){
@@ -134,7 +157,7 @@ class ModuleAViewController: UIViewController {
         //populates all possible peaks
         var possible_peaks: [Int: Float] = [:]
         for item in max_list{
-            if item.value >= 5{
+            if item.value >= 6{
                 possible_peaks[item.key] = max_list_val[item.key]
             }
         }
@@ -165,13 +188,24 @@ class ModuleAViewController: UIViewController {
         let harmonic_value = data[peak_index*2]
         let ratio_percent = harmonic_value/peak_value
 
-//        print("Harmonics: "+String(peak_index)+" and "+String(peak_index*2))
-//        print("Values: "+String(peak_value)+" and "+String(harmonic_value))
-//        print("Percent Ratio: "+String(ratio_percent)+" %")
-        if (ratio_percent > 0.10 && ratio_percent < 0.45) || (ratio_percent > 0.90 && ratio_percent < 1.00){
+        print("------------")
+        self.peak_sum = self.peak_sum+peak_value
+        self.peak_count = self.peak_count+1.0
+        self.harm_sum = self.harm_sum+harmonic_value
+        self.harm_count = self.harm_count+1.0
+//        print(peak_sum/peak_count)
+//        print(harm_sum/harm_count)
+        
+        
+        
+        
+        print("Percent Ratio: "+String(ratio_percent)+" %")
+        if (ratio_percent > 0.10 && ratio_percent < 0.42) || (ratio_percent > 0.80 && ratio_percent < 1.00){
+//            print("OOOOOOOOOOO")
             vowelLabel.text = "OOOOOOOOOO"
         }
-        if ratio_percent > 0.45 && ratio_percent < 0.90 {
+        else if (ratio_percent > 0.42 && ratio_percent < 0.80) || (ratio_percent > 0.00 && ratio_percent < 0.10){
+//            print("AAAAAAAAAA")
             vowelLabel.text = "AAAAAAAAAA"
         }
         
