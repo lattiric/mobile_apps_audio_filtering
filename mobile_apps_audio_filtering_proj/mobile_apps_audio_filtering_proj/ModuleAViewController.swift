7af72ee5c4e6f795eece207a0baa4d71ee1289fd
lattiric <78038846+lattiric@users.noreply.github.com>
2024-10-08 19:26:45 -0500

    Added Grad section

--- mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
+++ mobile_apps_audio_filtering_proj/mobile_apps_audio_filtering_proj/ModuleAViewController.swift
@@ -12,6 +12,7 @@ import Accelerate
 class ModuleAViewController: UIViewController {
     @IBOutlet weak var userView: UIView!
     
+    
     struct AudioConstants {
         static let AUDIO_BUFFER_SIZE = 1024 * 4
     }
@@ -23,8 +24,11 @@ class ModuleAViewController: UIViewController {
     
     @IBOutlet weak var freq2: UILabel!
     
+    @IBOutlet weak var vowelLabel: UILabel!
+    
     var cur_hz_1: Double = 0.0
     var cur_hz_2: Double = 0.0
+    var peak_1_index: Int = 0
     
     let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
     lazy var graph: MetalGraph? = {
@@ -36,7 +40,6 @@ class ModuleAViewController: UIViewController {
         self.view.backgroundColor = .white
         var run_counter = 0
         
-        // graph and audio processing (taken from original push, just moved to modules)
         setupGraph()
         audio.startMicrophoneProcessing(withFps: 20)
         audio.play()
@@ -70,14 +73,9 @@ class ModuleAViewController: UIViewController {
             graph.addGraph(withName: "fft",
                            shouldNormalizeForFFT: true,
                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE / 2)
-//            graph.addGraph(withName: "fft_zoomed",
-//                           shouldNormalizeForFFT: true,
-//                           numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE / 20)
+
             graph.addGraph(withName: "time",
                            numPointsInGraph: AudioConstants.AUDIO_BUFFER_SIZE)
-//            graph.addGraph(withName: "20 Pts Graph",
-//                           shouldNormalizeForFFT: true,
-//                           numPointsInGraph: 20)
 
             graph.makeGrids()
         }
@@ -87,26 +85,17 @@ class ModuleAViewController: UIViewController {
         if let graph = self.graph {
             graph.updateGraph(data: audio.fftData, forKey: "fft")
 
-//            let zoomedArray: [Float] = Array(audio.fftData[20...])
-//            graph.updateGraph(data: zoomedArray, forKey: "fft_zoomed")
-
             graph.updateGraph(data: audio.timeData, forKey: "time")
-
-//            var avgdArray: [Float] = []
-//            let chunkSize = audio.fftData.count / 20
-//            for num in 0...(19) {
-//                avgdArray.append(vDSP.maximum(audio.fftData[num * chunkSize...(num * chunkSize) + (chunkSize - 1)]))
-//            }
-//            graph.updateGraph(data: avgdArray, forKey: "20 Pts Graph")
         }
     }
     
     func updateLabels() {
         if let label1 = self.freq1, let label2 = self.freq2 {
             self.calcTone(audio_data: audio.fftData)
-            var output_label_1 = String(Int(cur_hz_1))
+            self.calcVowel(audio_data: audio.fftData, peak_index: peak_1_index)
+            let output_label_1 = String(Int(cur_hz_1))
             label1.text = output_label_1 + " Hz"
-            var output_label_2 = String(Int(cur_hz_2))
+            let output_label_2 = String(Int(cur_hz_2))
             label2.text = output_label_2 + " Hz"
         }
     
@@ -119,11 +108,11 @@ class ModuleAViewController: UIViewController {
     }
     
     func calcTone(audio_data: [Float]) {
-        var data: [Float] = audio_data
+        let data: [Float] = audio_data
         var window: [Float] = []
         var max_list: [Int: Int] = [:]
         var max_list_val: [Int: Float] = [:]
-        var hz_per_index = 44100.0/Double(AudioConstants.AUDIO_BUFFER_SIZE)
+        let hz_per_index = 44100.0/Double(AudioConstants.AUDIO_BUFFER_SIZE)
         
         for i in 1...(data.count-5) {
             window = Array(data[(i)...(i)+4])
@@ -150,13 +139,10 @@ class ModuleAViewController: UIViewController {
             }
         }
         
-//        print(max_list)
-//        print("break")
-//        print(possible_peaks)
-        
         // Get first highest frequency from dict
         if let (key, _) = possible_peaks.max(by: { $0.value < $1.value }){
-            cur_hz_1 = Double(key)*hz_per_index
+            cur_hz_1 = Double(key+1)*hz_per_index
+            peak_1_index = key
 //            print(String(key) + " " + String(cur_hz_1))
             possible_peaks.removeValue(forKey: key)
         } else {
@@ -171,8 +157,23 @@ class ModuleAViewController: UIViewController {
         } else {
             cur_hz_2 = Double(0.0)
         }
+    }
+    
+    func calcVowel(audio_data: [Float], peak_index: Int) {
+        let data: [Float] = audio_data
+        let peak_value = data[peak_index]
+        let harmonic_value = data[peak_index*2]
+        let ratio_percent = harmonic_value/peak_value
 
-      
+//        print("Harmonics: "+String(peak_index)+" and "+String(peak_index*2))
+//        print("Values: "+String(peak_value)+" and "+String(harmonic_value))
+//        print("Percent Ratio: "+String(ratio_percent)+" %")
+        if (ratio_percent > 0.10 && ratio_percent < 0.45) || (ratio_percent > 0.90 && ratio_percent < 1.00){
+            vowelLabel.text = "OOOOOOOOOO"
+        }
+        if ratio_percent > 0.45 && ratio_percent < 0.90 {
+            vowelLabel.text = "AAAAAAAAAA"
+        }
         
     }
     
